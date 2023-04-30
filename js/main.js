0939d57c64760227a621dc0d021bf80862f17ca3
tauri_s <tauristclaire@gmail.com>
2023-04-29 17:21:15 -0700

    JS function to show and hide dropdown menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-//Please add something like a floating scroll to top button implemented in JS to one of the pages.
+//show and hide dropdown list item on button click  
+function show_hide() {  
+    var click = document.getElementById("list-items");  
+    if(click.style.display ==="none") {  
+       click.style.display ="block";  
+    } else {  
+       click.style.display ="none";  
+    }   
+ }
